0cffc3faa9ee1868126d4a5f8c842ebcdb3ffc87
Cédric Dupont <lunarok@gmail.com>
2018-12-14 20:26:37 +0100

    update

--- core/class/mitsubishi.class.php
+++ core/class/mitsubishi.class.php
@@ -21,55 +21,21 @@ require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
 
 class mitsubishi extends eqLogic {
 
-
-  public function SetModif($option,$flag,$idflag){
-    log::add('mitsubishi', 'info', 'Modification '.$flag.' '.$idflag.' '.$option);
-
-    $montoken = config::byKey('MyToken', 'mitsubishi', '');
-    if ($montoken != '') {
-
-      $devideid = $this->getConfiguration('deviceid');
-      $buildid = $this->getConfiguration('buildid');
-      $typepac = $this->getConfiguration('typepac');
-
-      $request = new com_http('https://app.melcloud.com/Mitsubishi.Wifi.Client/Device/Get?id=' . $devideid . '&buildingID=' . $buildid);
-      $request->setHeader(array('X-MitsContextKey: ' . $montoken));
-      $json = $request->exec(30000, 2);
-      $device = json_decode($json, true);
-      $device[$flag] = $option;
-      $device['EffectiveFlags'] = $idflag;
-      $device['HasPendingCommand'] = 'true';
-
-
-      $ch = curl_init();
-
-      if ($typepac == 'air/eau'){
-        curl_setopt($ch, CURLOPT_URL, "https://app.melcloud.com/Mitsubishi.Wifi.Client/Device/SetAtw");
-      }else{
-        curl_setopt($ch, CURLOPT_URL, "https://app.melcloud.com/Mitsubishi.Wifi.Client/Device/SetAta");
-      }
-
-      curl_setopt($ch, CURLOPT_POST, 1);
-      curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-        'X-MitsContextKey: ' . $montoken,
-        'content-type: application/json'
-      ));
-      curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($device));
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-      $server_output = curl_exec($ch);
-      curl_close($ch);
-      $json = json_decode($server_output, true);
-      foreach ($this->getCmd() as $cmd) {
-        if ('NextCommunication' == $cmd->getName()) {
-          $cmd->setCollectDate('');
-          $time = strtotime($json['NextCommunication'] . " + 1 hours"); // Add 1 hour
-          $time = date('G:i:s', $time); // Back to string
-          $cmd->event($time);
-        }
-      }
-    }
+  public static function cron5() {
+    mitsubishi::refresh();
   }
 
+  public static function cronDaily() {
+    mitsubishi::gettoken();
+  }
+
+  public static function refresh() {
+
+  }
+
+  public function SetModif($option,$flag,$idflag){
+
+  }
 
   public static function gettoken() {
     $myemail = config::byKey('MyEmail', 'mitsubishi');
@@ -91,62 +57,8 @@ class mitsubishi extends eqLogic {
       config::save("MyToken", $json['LoginData']['ContextKey'], 'mitsubishi');
     } else {
       log::add('mitsubishi', 'debug', 'Login ou mot de passe Melcloud incorrecte.');
-      config::save("MyToken", $json['ErrorId'], 'mitsubishi');
     }
 
-  }
-
-  public static function cron5() {
-    mitsubishi::refresh();
-  }
-
-  public static function refresh() {
-    $montoken = config::byKey('MyToken', 'mitsubishi', '');
-    if ($montoken == '') {
-      mitsubishi::gettoken();
-    }
-    if ($montoken != '') {
-      log::add('mitsubishi', 'info', 'pull 5 minutes mytoken =' . $montoken);
-      //$request = new com_http('https://app.melcloud.com/Mitsubishi.Wifi.Client/Device/Get?id='.$devideid.'&buildingID='.$buildid);
-      $request = new com_http('https://app.melcloud.com/Mitsubishi.Wifi.Client/User/ListDevices');
-      $request->setHeader(array('X-MitsContextKey: ' . $montoken));
-      $json = $request->exec(30000, 2);
-
-      $values = json_decode($json, true);
-      foreach ($values as $maison) {
-        log::add('mitsubishi', 'debug', 'Maison ' . $maison['Name']);
-        for ($i = 0; $i < count($maison['Structure']['Devices']); $i++) {
-          log::add('mitsubishi', 'debug', 'pull : device 1 ' . $i . ' ' . $device['DeviceName']);
-          $device = $maison['Structure']['Devices'][$i];
-          self::pullCommande($device);
-        }
-        // FLOORS
-        for ($a = 0; $a < count($maison['Structure']['Floors']); $a++) {
-          log::add('mitsubishi', 'debug', 'FLOORS ' . $a);
-          // AREAS IN FLOORS
-          for ($i = 0; $i < count($maison['Structure']['Floors'][$a]['Areas']); $i++) {
-            for ($d = 0; $d < count($maison['Structure']['Floors'][$a]['Areas'][$i]['Devices']); $d++) {
-              $device = $maison['Structure']['Floors'][$a]['Areas'][$i]['Devices'][$d];
-              self::pullCommande($device);
-            }
-          }
-          // FLOORS
-          for ($i = 0; $i < count($maison['Structure']['Floors'][$a]['Devices']); $i++) {
-            $device = $maison['Structure']['Floors'][$a]['Devices'][$i];
-            self::pullCommande($device);
-          }
-        }
-        // AREAS
-        for ($a = 0; $a < count($maison['Structure']['Areas']); $a++) {
-          log::add('mitsubishi', 'info', 'AREAS ' . $a);
-          for ($i = 0; $i < count($maison['Structure']['Areas'][$a]['Devices']); $i++) {
-            log::add('mitsubishi', 'info', 'machine AREAS ' . $i);
-            $device = $maison['Structure']['Areas'][$a]['Devices'][$i];
-            self::pullCommande($device);
-          }
-        }
-      }
-    }
   }
 
   public function loadCmdFromConf($type) {
@@ -178,112 +90,6 @@ class mitsubishi extends eqLogic {
 			}
 		}
 	}
-
-  private static function pullCommande($device) {
-
-    log::add('mitsubishi', 'debug', 'pull : ' . $device['DeviceName']);
-    if ($device['DeviceID'] == '') return;
-    log::add('mitsubishi', 'debug', $device['DeviceID'] . ' ' . $device['DeviceName']);
-
-    $theEQlogic = eqLogic::byTypeAndSearhConfiguration('mitsubishi','"namemachine":"'.$device['DeviceName'].'"');
-    if (count($theEQlogic) == 0){
-
-      $mylogical = new mitsubishi();
-      $mylogical->setIsVisible(0);
-      $mylogical->setIsEnable(0);
-      $mylogical->setEqType_name('mitsubishi');
-      $mylogical->setName($device['DeviceName']);
-      $mylogical->setConfiguration('namemachine',$device['DeviceName']);
-      $mylogical->save();
-
-    }else{
-
-      $mylogical =  $theEQlogic[0];
-
-      if ($mylogical->getIsEnable()) {
-
-        log::add('mitsubishi', 'debug', 'setdevice ' . $device['Device']['DeviceID']);
-        $mylogical->setConfiguration('deviceid', $device['Device']['DeviceID']);
-        $mylogical->setConfiguration('buildid', $device['BuildingID']);
-
-        if ($device['Device']['DeviceType'] == '0'){
-          $mylogical->setConfiguration('typepac', 'air/air');
-          $mylogical->loadCmdFromConf('air');
-        }
-        if ($device['Device']['DeviceType'] == '1'){
-          $mylogical->setConfiguration('typepac', 'air/eau');
-          $mylogical->loadCmdFromConf('water');
-        }
-
-        $device['Device']['ListHistory24Formatters'] = '';
-
-
-        if ($mylogical->getConfiguration('rubriques') == '' )
-        {
-          $mylogical->setConfiguration('rubriques', print_r($device['Device'],true));
-        }
-
-        $mylogical->save();
-
-        foreach ($mylogical->getCmd() as $cmd) {
-
-          switch ($cmd->getLogicalId()) {
-
-            case 'OperationMode':
-            $cmd->setCollectDate('');
-
-            switch ($device['Device'][$cmd->getLogicalId()]){
-              case 7:
-              $cmd->event('Ventilation');
-              break;
-              case 1:
-              $cmd->event('Chauffage');
-              break;
-              case 2:
-              $cmd->event('Sechage');
-              break;
-              case 3:
-              $cmd->event('Froid');
-              break;
-              case 8:
-              $cmd->event('Automatique');
-              break;
-            }
-
-            break;
-
-
-            case 'Rafraichir':
-            log::add('mitsubishi', 'debug', 'log ' . $cmd->getLogicalId() . ' .On ne traite pas cette commande');
-            break;
-            default:
-
-            if ($cmd->getType() == 'action'){
-
-              log::add('mitsubishi', 'debug', 'log action '.$cmd->getName().' ' . $cmd->getLogicalId() . ' ' . $device['Device'][$cmd->getLogicalId()]);
-              $cmd->setConfiguration('lastCmdValue',$device['Device'][$cmd->getLogicalId()]);
-
-            }else{
-
-              log::add('mitsubishi', 'debug', 'log info '.$cmd->getName().' ' . $cmd->getLogicalId() . ' ' . $device['Device'][$cmd->getLogicalId()]);
-              if ('LastTimeStamp' == $cmd->getLogicalId()) {
-                $cmd->event(str_replace('T', ' ', $device['Device'][$cmd->getLogicalId()]));
-              } else {
-                $cmd->setCollectDate('');
-                $cmd->event($device['Device'][$cmd->getLogicalId()]);
-              }
-
-            }
-
-            $cmd->save();
-
-            break;
-          }
-
-        }
-      }
-    }
-  }
 
 }
 
