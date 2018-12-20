09f4e5ab2fa475400e86226b795a94feb6c38d9f
Cédric Dupont <lunarok@gmail.com>
2018-12-20 16:31:15 +0100

    first methods

--- core/class/mitsubishi.class.php
+++ core/class/mitsubishi.class.php
@@ -22,35 +22,32 @@ require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
 class mitsubishi extends eqLogic {
 
   public static function cron5() {
+    if (config::byKey('MyToken', 'melcloud', '') == '') {
+      mitsubishi::getToken();
+    }
     mitsubishi::refresh();
   }
 
-  public static function cronDaily() {
-    mitsubishi::gettoken();
-  }
-
   public static function refresh() {
-
+    $json = mitsubishi::callMelcloud('https://app.melcloud.com/Mitsubishi.Wifi.Client/User/ListDevices',array('X-MitsContextKey: ' . config::byKey('MyToken', 'melcloud', '')),array());
+    log::add('mitsubishi', 'debug', 'Retrive ' . print_r($json, true));
   }
 
   public function SetModif($option,$flag,$idflag){
 
   }
 
-  public static function gettoken() {
-    $myemail = config::byKey('MyEmail', 'mitsubishi');
-    $monpass = config::byKey('MyPassword', 'mitsubishi');
-
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, "https://app.melcloud.com/Mitsubishi.Wifi.Client/Login/ClientLogin");
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS,
-    "Email=" . $myemail . "&Password=" . $monpass . "&Language=7&AppVersion=1.10.1.0&Persist=true&CaptchaChallenge=null&CaptchaChallenge=null");
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    $server_output = curl_exec($ch);
-    curl_close($ch);
-
-    $json = json_decode($server_output, true);
+  public static function getToken() {
+    $data = array(
+      'Email' => config::byKey('MyEmail', 'mitsubishi'),
+      'Password' => config::byKey('MyPassword', 'mitsubishi'),
+      'Language' => '7',
+      'AppVersion' => '1.7.1.0',
+      'Persist' => 'true',
+      'CaptchaChallenge' => 'null',
+      'CaptchaResponse' => 'null'
+    );
+    $json = mitsubishi::callMelcloud('https://app.melcloud.com/Mitsubishi.Wifi.Client/Login/ClientLogin',array(),$data);
 
     if ($json['ErrorId'] == null) {
       log::add('mitsubishi', 'debug', 'Login ok ');
@@ -59,6 +56,19 @@ class mitsubishi extends eqLogic {
       log::add('mitsubishi', 'debug', 'Login ou mot de passe Melcloud incorrecte.');
     }
 
+  }
+
+  public static function callMelcloud($_url = '', $_header = array(), $_data = array()) {
+    $request_http = new com_http($_url);
+    if (!empty($_data)) {
+      $request_http->setPost($_data);
+    }
+    if (!empty($_header)) {
+      $request_http->setHeader($_header);
+    }
+    $request->setHeader(array('X-MitsContextKey: ' . $montoken));
+    $output = $request_http->exec(30);
+    return json_decode($output, true);
   }
 
   public function loadCmdFromConf($type) {
