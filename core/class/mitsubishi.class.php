43ed57370d15092a80d8351ef1d8f7f6790c1a05
lunarok@gmail.com <lunarok@gmail.com>
2019-01-09 23:31:34 +0100

    fix

--- core/class/mitsubishi.class.php
+++ core/class/mitsubishi.class.php
@@ -180,7 +180,7 @@ class mitsubishi extends eqLogic {
     $headers = array();
     $headers[] = 'Content-Type: application/json; charset=utf-8';
     $headers[] = 'X-Mitscontextkey: ' . config::byKey('token', 'mitsubishi');
-    $post="{\"DeviceId\":" . $this->getConfiguration('DeviceID') . ",\"FromDate\":\"" . date('Y-m-dTG:00:00', strtotime("1 day ago" )) . "\",\"ToDate\":\"" . date('Y-m-dTG:00:00') . "\",\"Duration\":1}";
+    $post="{\"DeviceId\":" . $this->getConfiguration('DeviceID') . ",\"FromDate\":\"" . date('Y-m-d\TG:00:00', strtotime("1 day ago" )) . "\",\"ToDate\":\"" . date('Y-m-d\TG:00:00') . "\",\"Duration\":1}";
     $json = mitsubishi::callMelcloud('https://app.melcloud.com/Mitsubishi.Wifi.Client/Report/GetOperationModeLog2',$headers,$post);
     log::add('mitsubishi', 'debug', 'Retrieve ' . $post);
     log::add('mitsubishi', 'debug', 'Retrieve ' . print_r($json, true));
