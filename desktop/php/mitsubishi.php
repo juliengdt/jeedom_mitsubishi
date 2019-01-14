0513aeb55dff0beba1ee90d6abbc40a41884419c
lunarok@gmail.com <lunarok@gmail.com>
2019-01-14 21:24:52 +0100

    conf page on eqlogic

--- desktop/php/mitsubishi.php
+++ desktop/php/mitsubishi.php
@@ -24,6 +24,16 @@ $eqLogics = eqLogic::byType('mitsubishi');
   </div>
 
   <div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
+    <legend><i class="fa fa-cog"></i>  {{Gestion}}</legend>
+    <div class="eqLogicThumbnailContainer">
+      <div class="cursor eqLogicAction" data-action="gotoPluginConf" style="background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;">
+        <center>
+          <i class="fa fa-wrench" style="font-size : 6em;color:#767676;"></i>
+        </center>
+        <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#767676"><center>{{Configuration}}</center></span>
+      </div>
+    </div>
+
     <legend>{{Mes Mitsubishi}}
     </legend>
     <div class="eqLogicThumbnailContainer">
