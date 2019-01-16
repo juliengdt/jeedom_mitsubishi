9b60474bc3d70365342f13361c09606c16755582
lunarok@gmail.com <lunarok@gmail.com>
2019-01-16 20:38:32 +0100

    modal

--- desktop/modal/stats.php
+++ desktop/modal/stats.php
@@ -9,27 +9,88 @@ if (!isConnect('admin')) {
 	<li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fa fa-tachometer"></i> {{Informations}}</a></li>
 </ul>
 
-<div class="tab-content" id="div_configuration">
-	<div role="tabpanel" class="tab-pane active" id="eqlogictab">
-		<br/>
-		<table class="table table-bordered tablesorter">
-			<thead>
-				<tr>
-					<th>{{Equipement}}</th>
-					<th>{{Plugin}}</th>
-					<th>{{Information}}</th>
-					<th>{{Trigger}}</th>
-				</tr>
-			</thead>
-			<tbody>
-			</tbody>
-		</table>
-	</div>
-
-</div>
+<div id="container" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
 
 
 <?php include_file('desktop', 'mitsubishi', 'js', 'mitsubishi');?>
 
 <script>
+
+// Create the chart
+Highcharts.chart('container', {
+  chart: {
+    type: 'column'
+  },
+  title: {
+    text: 'Statistiques de la veille'
+  },
+  xAxis: {
+    type: 'category'
+  },
+  yAxis: {
+    title: {
+      text: 'kWh'
+    }
+
+  },
+  legend: {
+    enabled: false
+  },
+  plotOptions: {
+    series: {
+      borderWidth: 0,
+      dataLabels: {
+        enabled: true,
+        format: '{point.y:.1f}kWh'
+      }
+    }
+  },
+
+  tooltip: {
+    headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+    pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
+  },
+
+  "series": [
+    {
+      "name": "Energie",
+      "colorByPoint": true,
+      "data": [
+				<?php
+				$eqLogic = mitsubishi::byLogicalId();
+				$cmd = mitsubishiCmd::byEqLogicIdAndLogicalId($eqLogic->getId(),'HotWater');
+				$HotWater = $cmd->execCmd();
+				$cmd = mitsubishiCmd::byEqLogicIdAndLogicalId($eqLogic->getId(),'Heating');
+				$Heating = $cmd->execCmd();
+				$cmd = mitsubishiCmd::byEqLogicIdAndLogicalId($eqLogic->getId(),'ProducedHotWater');
+				$ProducedHotWater = $cmd->execCmd();
+				$cmd = mitsubishiCmd::byEqLogicIdAndLogicalId($eqLogic->getId(),'ProducedHeating');
+				$ProducedHeating = $cmd->execCmd();
+				$cmd = mitsubishiCmd::byEqLogicIdAndLogicalId($eqLogic->getId(),'CoP');
+				$CoP = $cmd->execCmd();
+
+echo '
+				{
+					"name": "Eau Chaude énergie consommée",
+					"y": ' . $HotWater ',
+				},
+				{
+					"name": "Eau Chaude énergie produite",
+					"y": ' . $ProducedHotWater . ',
+				},
+				{
+					"name": "Chauffage énergie consommée",
+					"y": ' . $Heating . ',
+				},
+				{
+					"name": "Chauffage énergie produite",
+					"y": ' . $ProducedHeating . ',
+				}';
+
+				?>
+
+      ]
+    }
+  ]
+});
 </script>
