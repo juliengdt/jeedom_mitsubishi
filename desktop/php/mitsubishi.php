7513ea4816347872c27485783d0f4e4f6b9973b4
DUPONT <cedric.dupont@atos.net>
2020-01-17 12:19:54 +0100

    new look

--- desktop/php/mitsubishi.php
+++ desktop/php/mitsubishi.php
@@ -9,7 +9,7 @@ $eqLogics = eqLogic::byType('mitsubishi');
 ?>
 
 <div class="row row-overflow">
-  <div class="col-lg-2 col-sm-3 col-sm-4">
+  <div class="col-lg-2 col-sm-3 col-sm-4" id="hidCol" style="display: none;">
     <div class="bs-sidebar">
       <ul id="ul_eqLogic" class="nav nav-list bs-sidenav">
         <li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/></li>
@@ -22,26 +22,28 @@ $eqLogics = eqLogic::byType('mitsubishi');
     </div>
   </div>
 
-  <div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
+  <div class="col-lg-12 eqLogicThumbnailDisplay" id="listCol">
     <legend><i class="fas fa-cog"></i>  {{Gestion}}</legend>
-    <div class="eqLogicThumbnailContainer">
-      <div class="cursor eqLogicAction" data-action="gotoPluginConf" style="background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;">
-        <center>
-          <i class="fas fa-wrench" style="font-size : 6em;color:#767676;"></i>
-        </center>
-        <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#767676"><center>{{Configuration}}</center></span>
+    <div class="eqLogicThumbnailContainer logoPrimary">
+
+      <div class="cursor eqLogicAction logoSecondary" data-action="gotoPluginConf">
+        <i class="fas fa-wrench"></i>
+        <br/>
+        <span>{{Configuration}}</span>
       </div>
+
     </div>
 
-    <legend>{{Mes Mitsubishi}}
-    </legend>
+    <input class="form-control" placeholder="{{Rechercher}}" id="in_searchEqlogic" />
+
+    <legend><i class="fas fa-home" id="butCol"></i> {{Mes Equipements}}</legend>
     <div class="eqLogicThumbnailContainer">
       <?php
       foreach ($eqLogics as $eqLogic) {
         $opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
         echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff ; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;' . $opacity . '" >';
         echo "<center>";
-        echo '<img src="plugins/mitsubishi/plugin_info/mitsubishi_icon.png" height="105" width="95" />';
+        echo '<img src="plugins/vigilancemeteo/plugin_info/vigilancemeteo_icon.png" height="105" width="95" />';
         echo "</center>";
         echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>' . $eqLogic->getHumanName(true, true) . '</center></span>';
         echo '</div>';
